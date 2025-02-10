5f5f649281883a3ea41a8fa0861585c497c864f0
Carlos <carlos.soares@shcompanytechnology.com>
2025-02-10 01:16:28 -0300

    ajuste na animaçao das letras do texto de inicio

--- src/Body/Inicio/Inicio.jsx
+++ src/Body/Inicio/Inicio.jsx
@@ -5,7 +5,7 @@ export default function Inicio() {
 
 return (
     <div className="background-inicio text-center w-100 container-fluid">  
-        <h1 className='fontanallop' >
+        <h1 className='fontanallop'  >
             Wallison Guedes,
         </h1>
         <h1 className='fontanallop' id="text">
